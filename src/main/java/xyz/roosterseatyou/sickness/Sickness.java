9efce87b28cd314cfdbfd341140efc9a7055bbd4
Roosterseatyou <ltbkthree@icloud.com>
2022-06-18 15:34:20 -0500

    Setup :weary:

--- src/main/java/xyz/roosterseatyou/sickness/Sickness.java
+++ src/main/java/xyz/roosterseatyou/sickness/Sickness.java
@@ -1,17 +1,30 @@
 package xyz.roosterseatyou.sickness;
 
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Sickness extends JavaPlugin {
+    private static Plugin instance;
+    private static ComponentLogger logger;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        instance = this;
+        logger = getComponentLogger();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Plugin getInstance() {
+        return instance;
+    }
+
+    public static ComponentLogger logger() {
+        return logger;
     }
 }
