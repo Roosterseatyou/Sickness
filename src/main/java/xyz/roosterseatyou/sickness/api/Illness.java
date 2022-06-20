5c651d44a6187afd7786a09dc8cb5cd4eafabc1c
Roosterseatyou <ltbkthree@icloud.com>
2022-06-20 14:54:34 -0500

    Slight API addition

--- src/main/java/xyz/roosterseatyou/sickness/api/Illness.java
+++ src/main/java/xyz/roosterseatyou/sickness/api/Illness.java
@@ -1,5 +1,6 @@
 package xyz.roosterseatyou.sickness.api;
 
+import org.bukkit.entity.Player;
 import xyz.roosterseatyou.sickness.api.symptomhelp.SymptomHandler;
 
 import java.util.ArrayList;
@@ -9,12 +10,16 @@ public class Illness {
     private String name;
     private String description;
     private String cause;
+    private Player player;
+    private int durationInMinutes;
     private List<SymptomHandler> symptomHandlers = new ArrayList<>();
 
-    public Illness(String name, String description, String cause) {
+    public Illness(String name, String description, String cause, Player player, int durationInMinutes) {
         this.name = name;
         this.description = description;
         this.cause = cause;
+        this.player = player;
+        this.durationInMinutes = durationInMinutes;
     }
 
     public void register() {
@@ -33,6 +38,12 @@ public class Illness {
     public void removeSymptomHandler(SymptomHandler symptomHandler) {
         symptomHandler.unregister();
         symptomHandlers.remove(symptomHandler);
+    }
+
+    public void stopAffecting() {
+        for (SymptomHandler symptomHandler : symptomHandlers) {
+            symptomHandler.unregister();
+        }
     }
 
     public String getName() {
@@ -57,5 +68,21 @@ public class Illness {
 
     public void setCause(String cause) {
         this.cause = cause;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
     }
 }
