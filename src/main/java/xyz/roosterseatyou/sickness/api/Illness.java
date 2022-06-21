c7c13477d0a263c7bcfb130c6e6f168b32f41b1b
Roosterseatyou <ltbkthree@icloud.com>
2022-06-21 09:35:34 -0500

    Major API changes, yet again :skull:

--- src/main/java/xyz/roosterseatyou/sickness/api/Illness.java
+++ src/main/java/xyz/roosterseatyou/sickness/api/Illness.java
@@ -1,88 +1,42 @@
 package xyz.roosterseatyou.sickness.api;
 
 import org.bukkit.entity.Player;
+import xyz.roosterseatyou.sickness.api.symptomhelp.Symptom;
 import xyz.roosterseatyou.sickness.api.symptomhelp.SymptomHandler;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class Illness {
-    private String name;
-    private String description;
-    private String cause;
-    private Player player;
-    private int durationInMinutes;
-    private List<SymptomHandler> symptomHandlers = new ArrayList<>();
+public abstract class Illness {
+    private static SymptomHandler symptomHandler;
+    private ArrayList<Player> players = new ArrayList<>();
 
-    public Illness(String name, String description, String cause, Player player, int durationInMinutes) {
-        this.name = name;
-        this.description = description;
-        this.cause = cause;
-        this.player = player;
-        this.durationInMinutes = durationInMinutes;
+    protected abstract void register();
+
+    public void setSymptomHandler(SymptomHandler symptomHandler) {
+        Illness.symptomHandler = symptomHandler;
     }
 
-    public void register() {
-        Registry.registerIllness(this);
+    protected void addSymptom(Symptom symptom) {
+        symptomHandler.addSymptom(symptom);
     }
 
-    public void unregister() {
-        Registry.unregisterIllness(this);
+    public ArrayList<Player> getPlayers() {
+        return players;
     }
 
-    public void addSymptomHandler(SymptomHandler symptomHandler) {
-        symptomHandler.register();
-        symptomHandlers.add(symptomHandler);
+    public void updatePlayerList(ArrayList<Player> players) {
+        this.players = players;
     }
 
-    public void removeSymptomHandler(SymptomHandler symptomHandler) {
-        symptomHandler.unregister();
-        symptomHandlers.remove(symptomHandler);
+    public void infectPlayer(Player player) {
+        players.add(player);
     }
 
-    public void stopAffecting() {
-        for (SymptomHandler symptomHandler : symptomHandlers) {
-            symptomHandler.unregister();
-        }
+    public void removePlayer(Player player) {
+        players.remove(player);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getCause() {
-        return cause;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setCause(String cause) {
-        this.cause = cause;
-    }
-
-    public Player getPlayer() {
-        return player;
-    }
-
-    public void setPlayer(Player player) {
-        this.player = player;
-    }
-
-    public int getDurationInMinutes() {
-        return durationInMinutes;
-    }
-
-    public void setDurationInMinutes(int durationInMinutes) {
-        this.durationInMinutes = durationInMinutes;
+    public boolean isInfected(Player player) {
+        return players.contains(player);
     }
 }
